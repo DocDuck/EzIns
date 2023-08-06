7e8c355c4126f8c0b9d0716d142e0f0cd8cb119d
MitinAA <34368922+DocDuck@users.noreply.github.com>
2023-08-06 10:43:42 +0500

    Update App.js
    Check is button works

--- App.js
+++ App.js
@@ -1,37 +1,27 @@
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
-
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+import React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
 
 export default function App() {
+	const [focusSubject, setFocusSubject] = React.useState('');
+	const handlePress = () => {setFocusSubject("Здарова, приложуха считай почти готова)")};
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </View>
+		  <View style={styles.container}>
+				<Text>{focusSubject ?? 'Нажми на кнопку - получишь результат'}</Text>
+				<Button style={styles.button} onPress={handlePress}>Нажми на меня</Button>
+    	</View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+		flexDirection: "column",
+		justifyContent: "space-between",
     padding: 8,
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+	button: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+	}
 });
