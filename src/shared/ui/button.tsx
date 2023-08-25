7178e5d4c1a572ea1bbc69c6a9524316cc4a477c
MitinAA <34368922+DocDuck@users.noreply.github.com>
2023-08-25 13:00:32 +0500

    setup
    Добавил нужные зависимости под стор, стили, роутинг. Добавил компонент кнопки

---
+++ src/shared/ui/button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ViewProps } from 'react-native';
+import styled from 'styled-components/native';
+
+interface IButtonProps extends ViewProps {
+  // TODO theme?: ThemeInterface;
+  backgroundColor?: string;
+	onPress: () => void;
+	title: string;
+
+}
+
+const ButtonContainer = styled.TouchableOpacity<Omit<IButtonProps, 'title'>>`
+  margin-vertical: 40px;
+  width: 120px;
+  height: 40px;
+  padding: 12px;
+  border-radius: 10px;
+  background-color: ${props => props.backgroundColor};
+`;
+
+const ButtonText = styled.Text`
+  font-size: 16px;
+  text-align: center;
+`;
+
+const Button: React.FC<IButtonProps> = ({ onPress, backgroundColor, title }) => (
+  <ButtonContainer onPress={onPress} backgroundColor={backgroundColor}>
+    <ButtonText>{title}</ButtonText>
+  </ButtonContainer>
+);
+
+export default Button;
