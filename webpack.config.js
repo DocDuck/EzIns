afee0c5e0300198da15aa89b02eda57082c26713
MitinAA <34368922+DocDuck@users.noreply.github.com>
2023-09-02 10:35:29 +0500

    wip

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  // Customize the config before returning it.
+	  config.resolve.alias = {
+    ...config.resolve.alias,
+
+    'api': path.resolve(__dirname, 'src/api/'),
+    'pages': path.resolve(__dirname, 'src/pages/'),
+    'widgets': path.resolve(__dirname, 'src/widgets/'),
+    'features': path.resolve(__dirname, 'src/features/'),
+    'entities': path.resolve(__dirname, 'src/entities/'),
+    'shared': path.resolve(__dirname, 'src/shared/'),
+    'assets': path.resolve(__dirname, 'src/assets/'),
+	};
+  return config;
+};
