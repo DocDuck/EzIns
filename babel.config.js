f9f7ec1ac472c3b2495a24d2d78835584fc392c6
MitinAA <34368922+DocDuck@users.noreply.github.com>
2023-09-02 11:48:42 +0500

    +

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,22 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ["babel-preset-expo", "@babel/preset-typescript"],
     plugins: [
       [
         "module-resolver",
         {
-          root: ["./src/"],
+					extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx'
+          ],
+          root: ["./src"],
           alias: {
             app: "./src/app",
             pages: "./src/pages",
