fa13bf22abdcd663677dfc031363d96ac75882c2
MitinAA <34368922+DocDuck@users.noreply.github.com>
2023-09-02 13:14:43 +0500

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,14 +4,14 @@ import { Provider } from "react-redux";
 import { Routing } from "./routes";
 import { store } from "./store";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    // <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <Routing />
       </Provider>
-    </QueryClientProvider>
+    // </QueryClientProvider>
   );
 }
