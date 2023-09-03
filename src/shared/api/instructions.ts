b6ce72a0b3f26494f199037a419770fd6704e120
MitinAA <34368922+DocDuck@users.noreply.github.com>
2023-09-03 16:44:55 +0500

    file libs

--- src/shared/api/instructions.ts
+++ src/shared/api/instructions.ts
@@ -1,10 +1,14 @@
 import { INSTRUCTION_FOLDER } from "shared/config";
 import * as FileSystem from 'expo-file-system';
-import * as Permissions from 'expo-permissions';
-import * as MediaLibrary from 'expo-media-library';
-import * as Sharing from 'expo-sharing';
+// import * as Permissions from 'expo-permissions';
+// import * as MediaLibrary from 'expo-media-library';
+// import * as Sharing from 'expo-sharing';
+import { Logs } from 'expo'
+
+Logs.enableExpoCliLogging()
 
 const BASE_URL = "./assets/files";
+const TEMPLATE = "template.xlsx";
 
 export type Instruction = {
   id: number;
@@ -17,19 +21,19 @@ const getTable = async (): Promise<FileSystem.FileSystemDownloadResult | undefin
 	console.log('FileSystem.documentDirectory', FileSystem.documentDirectory);
         // Downloading the file
 		let res: FileSystem.FileSystemDownloadResult;		
-    let fileLocation = FileSystem.documentDirectory + BASE_URL + "template.xlsx";
+    let fileLocation = FileSystem.documentDirectory + BASE_URL + TEMPLATE;
     try {
 			res = await FileSystem.downloadAsync(BASE_URL, fileLocation)
-			console.log(res);
+			console.log('res', res);
         // Saving the file in a folder name `MyImages`
-        const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
-        if (status === "granted") {
-            const asset = await MediaLibrary.createAssetAsync(BASE_URL)
-            await MediaLibrary.createAlbumAsync("MyImages", asset, false)
-        }
+        // const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+        // if (status === "granted") {
+        //     const asset = await MediaLibrary.createAssetAsync(BASE_URL)
+        //     await MediaLibrary.createAlbumAsync("MyImages", asset, false)
+        // }
 
-        // Sharing the downloded file
-        Sharing.shareAsync(fileLocation);
+        // // Sharing the downloded file
+        // Sharing.shareAsync(fileLocation);
 				return res;
     }
 		catch ( error) {
